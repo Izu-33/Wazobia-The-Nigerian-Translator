aeb497b6294bf5afef02dbf7baaa55f8b3e335fd
Segun Odusina <odusinaoluwasegun@gmail.com>
2023-06-26 08:22:39 +0100

    Update wazobia.py for language

--- wazobia.py
+++ wazobia.py
@@ -1,5 +1,5 @@
-# import os
-# import openai
+import os
+import openai
 import streamlit as st
 from dotenv import load_dotenv
 from langchain.llms import OpenAI
@@ -49,7 +49,8 @@ trans_template = PromptTemplate(
 memory = ConversationBufferMemory(input_key='trans', memory_key='chat_history')
 
 # LLMs
-llm = OpenAI(model_name="text-davinci-003", temperature=0)
+llm = OpenAI(model_name="text-davinci-003", temperature=0, 
+             openai_api_key = "") # Ensure to input your OpenAI key here
 trans_chain = LLMChain(llm=llm, prompt=trans_template, verbose=True, output_key='translate', memory=memory)
 
 # If there's a prompt, process it and write out response on screen
@@ -57,46 +58,3 @@ if st.button("Translate"):
     if prompt:
         response = trans_chain({'trans': prompt})
         st.info(response['translate'])
-
-
-st.divider()
-
-st.markdown('### Wazobia PDFQuery')
-st.write('Wazobia PDFQuery can study your PDF file and answer questions about it.')
-
-pdf = st.file_uploader('Upload your PDF', type='pdf')
-
-if pdf:
-    pdf_reader = PdfReader(pdf)
-    text = ""
-
-    for page in pdf_reader.pages:
-        text += page.extract_text()
-
-    # Split into chunks
-    text_splitter = CharacterTextSplitter(
-        separator="\n",
-        chunk_size=1000,
-        chunk_overlap=200,
-        length_function=len
-    )
-
-    chunks = text_splitter.split_text(text)
-    
-    # Create embeddings
-    embeddings = OpenAIEmbeddings()
-    knowledge_base = FAISS.from_texts(chunks, embeddings)
-
-    # Collect user input
-    user_question = st.text_input("Ask Wazobia a question about your PDF")
-    if user_question:
-        docs = knowledge_base.similarity_search(user_question)
-        # View results of similarity search
-        # st.write(docs)
-
-        llm = OpenAI()
-        chain = load_qa_chain(llm, chain_type='stuff')
-        response = chain.run(input_documents=docs, question=user_question)
-
-        st.info(response)
-                
